e61b484aedc8faed7867768eeb9f2424e42fed55
waletaiwo <dewalebabz@gmail.com>
2023-03-29 23:43:17 +0100

    add terraform files

---
+++ main.tf
@@ -0,0 +1,24 @@
+provider "aws" {
+    resource "aws_s3_bucket_website_configuration" "example" {
+  bucket = aws_s3_bucket.example.id
+
+  index_document {
+    suffix = "index.html"
+  }
+
+  error_document {
+    key = "error.html"
+  }
+
+  routing_rule {
+    condition {
+      key_prefix_equals = "docs/"
+    }
+    redirect {
+      replace_key_prefix_with = "documents/"
+    }
+  }
+}
+
+    
+}
